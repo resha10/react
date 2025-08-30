8e530532e2f943c3a47de6c192bd4030c19d2c27
resha nakrani <reshanakrani@gmail.com>
2025-08-30 11:29:24 +0530

    exam pr

--- exam pr/src/main.jsx
+++ exam pr/src/main.jsx
@@ -1,32 +1,17 @@
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import App from "./App.jsx";
-// import { BrowserRouter } from "react-router-dom";
-// import { Provider } from "react-redux";
-// import { store }  from "./store.js"; 
-
-
-// createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     <Provider store={store}>
-//       <BrowserRouter>
-//         <App />
-//       </BrowserRouter>
-//     </Provider>
-//   </StrictMode>
-// );
-
-
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store.js";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
