986c40713283c2ec4289e5ff658563cfea1d91f8
resha nakrani <reshanakrani@gmail.com>
2025-09-11 15:32:22 +0530

    project-1

---
+++ project-1/server.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const fs = require('fs');
+
+
+const server = http.createServer((req, res) => {
+
+  let filename = "";
+  switch (req.url) {
+    case "/":
+      filename = "./index.html";
+      break;
+    case "/blog":
+      filename = "./blog.html";
+      break;
+    case "/contact":
+      filename = "./contact.html";
+      break;
+    case "/services":
+      filename = "./services.html";
+      break;
+    case "/about":
+      filename = "./about.html";
+      break;
+
+    default:
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.end("<h1>404 Page Not Found</h1>");
+      return;
+
+  }
+
+  const readfile = fs.readFileSync(filename, 'utf-8')
+  res.end(readfile)
+
+})
+
+server.listen(8010, () => {
+  console.log("Server is begin http://localhost:8010");
+});
